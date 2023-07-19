602c04521a0b52ba5381d730c591db8ef42bd6ba
Arabella Añora <anora.116775@caloocan.sti.edu.ph>
2023-07-19 14:12:45 +0800

    Hompage features-products-timer

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 function getTimeRemaining(endtime) {
-  var t = Date.parse(endtime) - Date.parse(new Date());
+  var t = Date.parse(endtime) - Date.now();
   var seconds = Math.floor((t / 1000) % 60);
   var minutes = Math.floor((t / 1000 / 60) % 60);
   var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
@@ -37,5 +37,21 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 30 * 24 * 60 * 60 * 1000);
+var storedDeadline = localStorage.getItem('deadline');
+var deadline;
+
+if (storedDeadline) {
+  deadline = new Date(storedDeadline);
+  if (deadline < new Date()) {
+    // The stored deadline has already passed, set a new deadline
+    deadline = new Date();
+    deadline.setDate(deadline.getDate() + 30); // Set new deadline to 30 days from now
+    localStorage.setItem('deadline', deadline); // Update the stored deadline in local storage
+  }
+} else {
+  deadline = new Date();
+  deadline.setDate(deadline.getDate() + 30); // Set initial deadline to 30 days from now
+  localStorage.setItem('deadline', deadline); // Store the initial deadline in local storage
+}
+
 initializeClock('clockdiv', deadline);
